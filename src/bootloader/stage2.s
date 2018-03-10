f19f9f6b4e1ece64a355a155c9bfe9f5be673db9
zelinf <fengzlin@mail2.sysu.edu.cn>
2018-03-10 21:06:22 +0800

    Implement delay

--- src/bootloader/stage2.s
+++ src/bootloader/stage2.s
@@ -21,7 +21,37 @@ main:
     mov si, msg
     call print
 
+    call waitForASecond
+
+    mov si, msg
+    call print
+
+    mov si, greeting
+    call print
+
     cli
     hlt
 
-msg db "Preparing to load operating system...",13, 10, 0
+    ; start waiting
+waitForASecond:
+    push ax
+    push bx
+    push dx
+    mov ah, 00h
+    int 1ah
+    mov bx, dx
+.waiting:
+    mov ah, 00h
+    int 1ah
+    sub dx, bx
+    cmp dx, 18 ;wait for 18 * 55ms
+    jle .waiting
+    pop dx
+    pop bx
+    pop ax
+    ret
+    ; end waiting
+
+msg db "Preparing to load operating system...", 13, 10, 0
+
+greeting db "Hello!", 13, 10, 0
