af136c580feac78566d5bfc69424429e34431829
zelinf <fengzlin@mail2.sysu.edu.cn>
2018-03-14 07:48:35 +0800

    Enable A20

--- src/bootloader/stage2.s
+++ src/bootloader/stage2.s
@@ -33,20 +33,32 @@ main:
     cli
     ; load GDT
     lgdt [toc]
+    ; enable A20 (so that we can use 32 bit address space)
+    call enable_a20
     ; enter protected mode
     mov eax, cr0
     or eax, 1
     mov cr0, eax
     jmp 0x08:stage3 ; fix 'cs'
 
+enable_a20:
+    .wait_input:
+        in al, 0x64
+        test al, 2 ; input buffer is empty
+        jnz .wait_input
+    mov al, 0xdd ; enable A20
+    out 0x64, al
+    ret
+
 BITS 32 ; We make it into 32 bit!
 stage3:
+    ; set data segments to data selector
     mov ax, 0x10
     mov ds, ax
     mov ss, ax
     mov es, ax
-    mov esp, 0x90000 ; stack's base address
-
+    ; set the base address of stack
+    mov esp, 0x90000
 stop:
     cli
     hlt
