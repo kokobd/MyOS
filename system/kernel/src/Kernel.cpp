5c35b5fa36fcd1409edbb513a17fea642e132b8d
zelinf <fengzlin@mail2.sysu.edu.cn>
2018-04-15 18:18:30 +0800

    Automatically initialize all global objects before main

--- system/kernel/src/Kernel.cpp
+++ system/kernel/src/Kernel.cpp
@@ -31,8 +31,39 @@ extern "C" void __cxa_pure_virtual() {
     // No op
 }
 
-extern "C" int c_main() {
-    extern int main();
+namespace {
 
+class Dumb {
+public:
+    Dumb() {
+        asm volatile("nop");
+    }
+};
+
+// So that we can make sure .init_array section exists.
+Dumb dumb;
+
+}
+
+extern "C" uint32_t __init_array_start;
+
+extern "C" {
+
+int c_main() {
+    extern int main();
+    auto ctor = reinterpret_cast<void (*)()>(__init_array_start);
+    ctor();
     return main();
+}
+
+void *__dso_handle;
+
+int __cxa_atexit(void (*destructor)(void *), void *arg, void *dso) {
+    return 0;
+}
+
+void __cxa_finalize(void *f) {
+
+}
+
 }
