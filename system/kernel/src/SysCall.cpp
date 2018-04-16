1fd172fb518627230650f9ac5e36735d9d881e72
zelinf <fengzlin@mail2.sysu.edu.cn>
2018-04-16 10:51:48 +0800

    Remove legacy code

--- system/kernel/src/SysCall.cpp
+++ system/kernel/src/SysCall.cpp
@@ -1,8 +1,6 @@
 #include <myos/kernel/SysCall.hpp>
 #include <myos/kernel/cpu/CPU.hpp>
 #include <myos/kernel/Kernel.hpp>
-#include "shell.h"
-#include "filesystem/file.h"
 
 namespace myos::kernel {
 
@@ -29,27 +27,6 @@ uint32_t SysCall::InterruptHandlerImpl::runSysCall(
         uint32_t arg3) {
     uint32_t ret = 0xFFFFFFFF;
     switch (id) {
-        case 0:
-            ret = static_cast<uint32_t>(kernel_filesystem_file_fopen((const char *) arg1, (uint32_t) arg2));
-            break;
-        case 1:
-            ret = static_cast<uint32_t>(kernel_filesystem_file_fclose(arg1));
-            break;
-        case 2:
-            ret = static_cast<uint32_t>(kernel_filesystem_file_fread(arg1, (uint8_t *) arg2, (size_t) arg3));
-            break;
-        case 3:
-            kernel_shell_termPutChar(kernel_shell_getGlobalTerminal(), (char) arg1);
-            ret = 0;
-            break;
-        case 4:
-            ret = static_cast<uint32_t>(kernel_shell_termGetChar(kernel_shell_getGlobalTerminal()));
-            break;
-        case 5:
-            ret = kernel_filesystem_file_fGetSize(arg1);
-            break;
-        case 6:
-            ret = static_cast<uint32_t>((int32_t) kernel_shell_getGlobalTerminal()->arg);
         default:
             break;
     }
--- system/kernel/src/main.cpp
+++ system/kernel/src/main.cpp
@@ -1,36 +1,7 @@
-#include <cstdint>
-#include <cstring>
-#include <cctype>
-
 #include <myos/kernel/Kernel.hpp>
-#include "keyboard/input.h"
-#include "shell.h"
-#include "filesystem/file.h"
-#include "exec/elf32.h"
-// TODO remove this include
-#include <myos/kernel/cpu/InterruptHandler.hpp>
-
-static void initAll();
-
-static void executeApplication(kernel_shell_Terminal *terminal, const char *name);
 
 using myos::kernel::Kernel;
 using namespace myos::kernel;
-
-class ClockHandler : public cpu::InterruptHandler {
-public:
-    explicit ClockHandler(kernel_shell_Terminal *terminal) : ch('0'), terminal(terminal) {}
-    void handleInterrupt(cpu::InterruptType interrupt, cpu::RegisterState &registerState) override {
-        kernel_shell_termPutChar(terminal, ch);
-
-        if (ch == '9')
-            ch = '0';
-        else ++ch;
-    }
-private:
-    char ch;
-    kernel_shell_Terminal *terminal;
-};
 
 void userFunc() {
     asm volatile (
@@ -44,66 +15,5 @@ int main() {
     kernel.getCPU().enterUserCode(userProgram,
                                   (void *) 0x300000);
 
-//    kernel_shell_Terminal *terminal = kernel_shell_getGlobalTerminal();
-//    kernel_shell_termInit(terminal);
-//    kernel_shell_termPutString(terminal, "Hello\n");
-//
-//    ClockHandler clockHandler(terminal);
-//    kernel.getCPU().registerInterruptHandler(cpu::InterruptType::TIMER, &clockHandler);
-//#pragma clang diagnostic push
-//#pragma clang diagnostic ignored "-Wmissing-noreturn"
-//    while (true) {
-//        asm volatile ( "nop" );
-//    }
-//#pragma clang diagnostic pop
-//
-//    size_t INPUT_LIMIT = 120;
-//    char input[INPUT_LIMIT];
-//    memset(input, 0, INPUT_LIMIT);
-//    while (strcmp(input, "EXIT") != 0) {
-//        kernel_shell_termPutString(terminal, "MyOS 0.1> ");
-//        memset(input, 0, INPUT_LIMIT);
-//        size_t inputSize = kernel_shell_termReadLine(terminal, input, INPUT_LIMIT - 1);
-//
-//        const char *progNameEnd;
-//        for (progNameEnd = input; *progNameEnd != '\0'; ++progNameEnd) {
-//            if (isspace(*progNameEnd))
-//                break;
-//        }
-//
-//        char progName[80];
-//        memset(progName, 0, 80);
-//        memcpy(progName, input, progNameEnd - input);
-//
-//        const char *suffix = ".exe";
-//        memcpy(progName + strlen(progName), suffix, strlen(suffix));
-//
-//        for (char *pt = progName; *pt != '\0'; ++pt) {
-//            *pt = (char) toupper(*pt);
-//        }
-//
-//        kernel_shell_termSetArg(terminal, input);
-//        executeApplication(terminal, progName);
-//    }
-//
-//    kernel_shell_termPutString(terminal, "Exited.\n");
-//    asm(
-//    "cli\n"
-//    "hlt"
-//    );
-}
-
-static void executeApplication(kernel_shell_Terminal *terminal, const char *name) {
-    int32_t fileHandle = kernel_filesystem_file_fopen(name, 0);
-    if (fileHandle >= 0) {
-        uint8_t *image = (uint8_t *) 0x200000;
-        const size_t sizeLimit = 0x100000;
-        int ret = kernel_filesystem_file_fread(fileHandle, image, sizeLimit);
-        kernel_exec_elf32_execute(image);
-        kernel_filesystem_file_fclose(fileHandle);
-    } else {
-        kernel_shell_termPutString(terminal, "Can not find executable ");
-        kernel_shell_termPutString(terminal, name);
-        kernel_shell_termPutChar(terminal, '\n');
-    }
+    return 0;
 }
