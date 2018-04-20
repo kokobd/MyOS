febfbcd450422f6eb43067e2a7066811e721e883
zelinf <fengzlin@mail2.sysu.edu.cn>
2018-04-21 00:39:02 +0800

    Refine display

--- apps/spin.c
+++ apps/spin.c
@@ -6,10 +6,22 @@ int main() {
     uint32_t flag = 0;
     uint32_t r = 0;
     uint32_t c = 0;
+    uint32_t rPrev = 0;
+    uint32_t cPrev = 0;
     char ch = 'X';
+    for (int i = 0; i < 25; ++i)
+    {
+        for (int j = 0; j < 80; ++j)
+        {
+            setChar(i, j, ' ');
+        }
+    }
+
     for (int i = 0; i < 50000; ++i) {
         setChar(r, c, ch);
         sleep();
+        rPrev = r;
+        cPrev = c;
         flag++;
         if (flag > 0 && flag < 80) {
             c++;
@@ -22,6 +34,7 @@ int main() {
         } else if (flag == 207) {
             flag = 0;
         }
+        setChar(rPrev, cPrev, ' ');
     }
     return 0;
 }
